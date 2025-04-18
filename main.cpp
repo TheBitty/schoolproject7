1a8c92049c565238efd4e7e8eb45003482df53fa
TheBitty <hitsuu23@gmail.com>
2025-04-18 00:27:22 -0700

    code is whatever just getting the project started

--- main.cpp
+++ main.cpp
@@ -4,3 +4,17 @@ class linkedListDataType
 {
     void divideAt(linkedListDataType<Type> linked_list, int postion)
     };
+
+////Divide the list at the node with the info item into two
+//sublists.
+//Postcondition: first and last point to the first and
+// last nodes of the first sublist.
+// secondList.first and secondList.last
+// point to the first and last nodes of the
+// second sublist.
+//unorderedLinkedList<int> myList;
+//unorderedLinkedList<int> otherList;
+
+//divides myList into two sublists: myList points to the list with the
+//elements 34 65,and otherList points to the sublist with the elements
+//18 39 27 89 12.
